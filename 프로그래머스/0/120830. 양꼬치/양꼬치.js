8e997c85d3b0a72077fd53e798cac749af0132b0
Rina <152532049+kzlsk@users.noreply.github.com>
2025-08-04 22:58:01 +0900

    [level 0] Title: 양꼬치, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,17 +1,6 @@
 function solution(n, k) {
-    let sum = 0;
-    let free = 0;
-    let juice = 0;
-    let food = 0;
-    if(n >= 10){
-        free = ~~(n / 10) * 2000;
-        food = n * 12000;
-        juice = k * 2000;
-        sum = (food + juice) - free;
-    } else {
-        food = n * 12000;
-        juice = k * 2000;
-        sum = food + juice;
-    }
-    return sum;
+    var answer = 0;
+    let s = ~~(n / 10);
+    answer = n * 12000 + k * 2000 - s * 2000;
+    return answer;
 }
