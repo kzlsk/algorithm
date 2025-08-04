78cef545807886e2aa4990d0811cf75921b3072e
Rina <152532049+kzlsk@users.noreply.github.com>
2025-08-04 23:12:04 +0900

    [level 0] Title: 짝수의 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,9 +1,9 @@
 function solution(n) {
-    let sum = 0;
-    for(let i = 0; i <= n; i++){
-        if(i % 2 == 0){
-            sum += i;
-        }
+    var answer= 0;
+    for(let i = 1; i <= n; i++){
+        if(i % 2 === 0){
+            answer += i;
+        } 
     }
-    return sum;
+    return answer;
 }
