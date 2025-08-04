87657709bcea1ed1ee30020e51223ca8ee1223be
Rina <152532049+kzlsk@users.noreply.github.com>
2025-08-04 22:15:49 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,10 +1,4 @@
 function solution(num1, num2) {
-    var answer;
-    
-    if(num1 === num2){
-        answer = 1;
-    } else {
-        answer = -1;
-    }
+    var answer = (num1 === num2 ? 1 : -1);
     return answer;
 }
