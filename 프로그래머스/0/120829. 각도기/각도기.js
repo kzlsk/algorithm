429e9973ff36b6bbbb54af708b0908286558d994
Rina <152532049+kzlsk@users.noreply.github.com>
2025-08-04 22:44:31 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,13 +1,10 @@
 function solution(angle) {
-    var answer = 0;
-    if(angle > 0 && angle < 90){
-        answer = 1;
-    } else if(angle == 90){
-        answer = 2;
-    } else if(angle > 90 && angle <180){
-        answer = 3;
-    } else if(angle == 180){
-        answer = 4;
+    let answer;
+    switch(true){
+        case(angle > 0 && angle < 90): answer = 1; break;
+        case(angle === 90): answer = 2;  break;
+        case(angle > 90 && angle < 180): answer = 3; break;
+        case(angle === 180): answer = 4; break;
     }
     return answer;
 }
