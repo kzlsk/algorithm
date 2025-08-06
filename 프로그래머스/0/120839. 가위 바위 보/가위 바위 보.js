8790f72b9c243e1ad78a143bc5629a805cc653ac
Rina <152532049+kzlsk@users.noreply.github.com>
2025-08-07 01:01:49 +0900

    [level 0] Title: 가위 바위 보, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,4 +1,5 @@
 function solution(rsp) {
-    let result = [];
-    return rsp.split('').map(cur => cur === '2' ? '0' : cur === '0' ? '5' : '2').join('');
+	let arr = {2: 0, 0: 5, 5: 2};
+	var answer = [...rsp].map(v => arr[v]).join('');
+	return answer;
 }
