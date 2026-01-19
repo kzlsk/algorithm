75985649d150fb4cd7a49126204718409fcea8f2
Rina <152532049+kzlsk@users.noreply.github.com>
2026-01-19 21:54:26 +0900

    [level 1] Title: 예산, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,16 @@
+function solution(d, budget) {
+    var answer = 0;
+    let sum = 0;
+    
+    d.sort((a, b) => a - b);
+    
+    for(let i = 0; i < d.length; i++){
+        if(sum + d[i] <= budget){
+            sum += d[i];
+            answer++;
+        } else {
+            break;
+        }
+    }
+    return answer;
+}
