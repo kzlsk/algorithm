7a612d0526449c3dc67dcceaa19b717fa61688f9
Rina <152532049+kzlsk@users.noreply.github.com>
2026-02-22 17:04:51 +0900

    [Bronze V] Title: 시험 성적, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.js
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -2,21 +2,13 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 
 const score = input[0];
-switch(true){
-        case(score >= 90):
-            console.log('A')
-            break;
-        case(score >= 80):
-            console.log('B')
-            break;
-        case(score >= 70):
-            console.log('C')
-            break;
-        case(score >= 60):
-            console.log('D')
-            break;
-        default:
-            console.log('F')
-            break;
-}
-
+if (score >= 90)
+	console.log('A');
+else if (score >= 80)
+	console.log('B');
+else if(score >= 70)
+	console.log('C');
+else if(score >= 60)
+	console.log('D');
+else 
+	console.log('F');
